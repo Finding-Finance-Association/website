2e456c69406a2e807b45499f33179bd0421a8ec5
1-apex <pratham153patil@gmail.com>
2025-05-23 10:16:09 -0500

    init

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function ContactPage() {
+  return (
+    <div className="flex flex-col gap-4">
+      <h1 className="text-2xl font-bold">Contact</h1>
+      <p> Content </p>
+    </div>
+  );
+}
