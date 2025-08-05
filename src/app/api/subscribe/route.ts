867dc012f8a59e80dfe7d6ec18232d5a7c44f0ee
Char Z <2003charz@gmail.com>
2025-08-04 23:56:02 -0700

    double checking functionality (not tested, no access to mailchip atm)

--- src/app/api/subscribe/route.ts
+++ src/app/api/subscribe/route.ts
@@ -52,6 +52,9 @@ export async function POST(request: Request) {
       );
     }
 
-    return NextResponse.json({ error: 'Internal server error. Failed to subscribe.' }, { status: 500 });
+    return NextResponse.json(
+      { error: 'Internal server error. Failed to subscribe.' }, 
+      { status: 500 }
+    );
   }
 }
